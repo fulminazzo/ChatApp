6d3ed5b6ae21c03f9c8a9b736543412143b7d92f
user <fulminazzo21@gmail.com>
2025-01-26 22:19:04 +0100

    Created User entity

---
+++ src/main/java/it/fulminazzo/chatapp/backend/domain/entities/User.java
@@ -0,0 +1,27 @@
+package it.fulminazzo.chatapp.backend.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
