1eafd18bcd6e360bb17321ce5ddf6b43f3db6f03
user <fulminazzo21@gmail.com>
2025-01-26 22:48:47 +0100

    Created ChatUserDetails

---
+++ src/main/java/it/fulminazzo/chatapp/backend/security/ChatUserDetails.java
@@ -0,0 +1,28 @@
+package it.fulminazzo.chatapp.backend.security;
+
+import it.fulminazzo.chatapp.backend.domain.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public record ChatUserDetails(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+}
