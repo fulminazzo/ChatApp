823d4d35c185b7e0261dffdd213cb66b5833cc46
user <fulminazzo21@gmail.com>
2025-01-26 23:33:51 +0100

    Added tests for ChatUserDetailsService

---
+++ src/test/groovy/it/fulminazzo/chatapp/backend/security/services/ChatUserDetailsServiceTest.groovy
@@ -0,0 +1,48 @@
+package it.fulminazzo.chatapp.backend.security.services
+
+import it.fulminazzo.chatapp.backend.domain.entities.User
+import it.fulminazzo.chatapp.backend.repositories.UserRepository
+import it.fulminazzo.chatapp.backend.security.objects.ChatUserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import spock.lang.Specification
+
+class ChatUserDetailsServiceTest extends Specification {
+    private def user
+    private ChatUserDetailsService service
+
+    void setup() {
+        user = User.builder()
+                .id(UUID.randomUUID())
+                .username('fulminazzo')
+                .password('password')
+                .build()
+        UserRepository userRepository = Mock()
+        userRepository.findByUsername(user.username) >> Optional.of(user)
+        userRepository.findByUsername(_) >> Optional.empty()
+        service = new ChatUserDetailsService(userRepository)
+    }
+
+    def 'found user should return correct user details'() {
+        given:
+        def expected = new ChatUserDetails(user)
+
+        when:
+        def actual = service.loadUserByUsername(user.username)
+
+        then:
+        actual == expected
+    }
+
+    def 'not found user should throw exception with username'() {
+        given:
+        def username = 'fulminazzoeviltwin'
+
+        when:
+        service.loadUserByUsername(username)
+
+        then:
+        def e = thrown(UsernameNotFoundException)
+        e.message == username
+    }
+
+}
