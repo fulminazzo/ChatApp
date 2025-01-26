45c3f7635f72df0d1d8dc16925b5fd075f4777a0
user <fulminazzo21@gmail.com>
2025-01-26 23:11:23 +0100

    Extracted IChatUserDetailsService interface

--- src/main/java/it/fulminazzo/chatapp/backend/security/services/ChatUserDetailsService.java
+++ src/main/java/it/fulminazzo/chatapp/backend/security/services/ChatUserDetailsService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 //@RequiredArgsConstructor
-public class ChatUserDetailsService implements UserDetailsService {
+class ChatUserDetailsService implements IChatUserDetailsService {
     private final UserRepository userRepository;
 
     //TODO: temporary
---
+++ src/main/java/it/fulminazzo/chatapp/backend/security/services/IChatUserDetailsService.java
@@ -0,0 +1,7 @@
+package it.fulminazzo.chatapp.backend.security.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface IChatUserDetailsService extends UserDetailsService {
+
+}
