60dae3d8e53bcd7413274b98a42583d9f666e020
user <fulminazzo21@gmail.com>
2025-01-28 00:06:50 +0100

    Removed unused springboot test

--- src/test/groovy/it/fulminazzo/chatapp/backend/security/utils/JwtProviderTest.groovy
+++ src/test/groovy/it/fulminazzo/chatapp/backend/security/utils/JwtProviderTest.groovy
@@ -2,10 +2,8 @@ package it.fulminazzo.chatapp.backend.security.utils
 
 import io.jsonwebtoken.Jwts
 import it.fulminazzo.chatapp.backend.security.config.JwtKeyConfig
-import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
-@SpringBootTest
 class JwtProviderTest extends Specification {
 
     private static final SECRET_KEY = 'SGVsbG8sIHdvcmxkISBUaGlzIGlzIGEgc3VwZXIgc2VjdXJlIG1lc3NhZ2UgdGhhdCBub2JvZHkgd2lsbCBiZSBhYmxlIHRvIGRlY29kZS4KVW5sZXNzIHlvdSBleHBsaWNpdGx5IHdhbnQgdG8gZ2V0IGhhY2tlZCwgeW91IHNob3VsZCBjaGFuZ2UgdGhpcyBrZXkgaW1tZWRpYXRlbHkuClBsZWFzZSwgRE8gTk9UIFVTRSBNRSBJTiBQUk9EVUNUSU9OLiBFVkVSLg'
