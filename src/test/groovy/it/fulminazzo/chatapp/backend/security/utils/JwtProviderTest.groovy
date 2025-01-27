1af70566a57458ded62e8d55fda5ab98d50a17bf
user <fulminazzo21@gmail.com>
2025-01-28 00:11:53 +0100

    Added tests for invalid token

--- src/test/groovy/it/fulminazzo/chatapp/backend/security/utils/JwtProviderTest.groovy
+++ src/test/groovy/it/fulminazzo/chatapp/backend/security/utils/JwtProviderTest.groovy
@@ -2,6 +2,7 @@ package it.fulminazzo.chatapp.backend.security.utils
 
 import io.jsonwebtoken.Jwts
 import it.fulminazzo.chatapp.backend.security.config.JwtKeyConfig
+import it.fulminazzo.chatapp.backend.security.exceptions.InvalidJwtException
 import spock.lang.Specification
 
 class JwtProviderTest extends Specification {
@@ -28,6 +29,26 @@ class JwtProviderTest extends Specification {
     }
 
     private String generateToken() {
+        return Jwts.builder()
+                .subject('fulminazzo')
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 100000))
+                .signWith(provider.secretKey)
+                .compact()
+    }
+
+    def 'test that expired jwt throws exception'() {
+        given:
+        def token = generateExpiredToken()
+
+        when:
+        provider.extractUsernameFromJwtToken(token)
+
+        then:
+        thrown(InvalidJwtException)
+    }
+
+    private String generateExpiredToken() {
         return Jwts.builder()
                 .subject('fulminazzo')
                 .issuedAt(new Date(System.currentTimeMillis()))
