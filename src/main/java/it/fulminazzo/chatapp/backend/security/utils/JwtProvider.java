0a855b22f1c45215f050c887b42b2cee50faf230
user <fulminazzo21@gmail.com>
2025-01-28 00:27:33 +0100

    Added generateJwtTokenFromUsername method

--- src/main/java/it/fulminazzo/chatapp/backend/security/utils/JwtProvider.java
+++ src/main/java/it/fulminazzo/chatapp/backend/security/utils/JwtProvider.java
@@ -1,6 +1,5 @@
 package it.fulminazzo.chatapp.backend.security.utils;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import it.fulminazzo.chatapp.backend.security.config.JwtKeyConfig;
@@ -11,11 +10,13 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
 
+    public static final String ALGORITHM = "HmacSHA256";
     private final JwtKeyConfig jwtKeyConfig;
 
     public String extractUsernameFromJwtToken(String token) throws InvalidJwtException {
@@ -31,9 +32,18 @@ public class JwtProvider {
         }
     }
 
+    public String generateJwtTokenFromUsername(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(null)
+                .signWith(getSecretKey())
+                .compact();
+    }
+
     public SecretKey getSecretKey() {
         byte[] keyBytes = Base64.getDecoder().decode(jwtKeyConfig.getSecretKey());
-        return new SecretKeySpec(keyBytes, "HmacSHA256");
+        return new SecretKeySpec(keyBytes, ALGORITHM);
     }
 
 }
