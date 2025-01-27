1b9dd5ccbffe3604b6afbd95bdb20fab04b9f62a
user <fulminazzo21@gmail.com>
2025-01-28 00:15:42 +0100

    Added validateToken method

--- src/main/java/it/fulminazzo/chatapp/backend/security/services/AuthenticationService.java
+++ src/main/java/it/fulminazzo/chatapp/backend/security/services/AuthenticationService.java
@@ -1,5 +1,7 @@
 package it.fulminazzo.chatapp.backend.security.services;
 
+import it.fulminazzo.chatapp.backend.security.exceptions.InvalidJwtException;
+import it.fulminazzo.chatapp.backend.security.utils.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,6 +15,8 @@ class AuthenticationService implements IAuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final IChatUserDetailsService userDetailsService;
 
+    private final JwtProvider jwtProvider;
+
     @Override
     public UserDetails authenticate(String username, String password) {
         authenticationManager.authenticate(
@@ -24,6 +28,12 @@ class AuthenticationService implements IAuthenticationService {
     @Override
     public UserDetails register(String username, String password) {
         return userDetailsService.createUser(username, password);
+    }
+
+    @Override
+    public UserDetails validateToken(String token) throws InvalidJwtException {
+        String username = jwtProvider.extractUsernameFromJwtToken(token);
+        return userDetailsService.loadUserByUsername(username);
     }
 
 }
--- src/main/java/it/fulminazzo/chatapp/backend/security/services/IAuthenticationService.java
+++ src/main/java/it/fulminazzo/chatapp/backend/security/services/IAuthenticationService.java
@@ -1,5 +1,6 @@
 package it.fulminazzo.chatapp.backend.security.services;
 
+import it.fulminazzo.chatapp.backend.security.exceptions.InvalidJwtException;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface IAuthenticationService {
@@ -7,5 +8,7 @@ public interface IAuthenticationService {
     UserDetails authenticate(String username, String password);
 
     UserDetails register(String username, String password);
+
+    UserDetails validateToken(String token) throws InvalidJwtException;
 
 }
