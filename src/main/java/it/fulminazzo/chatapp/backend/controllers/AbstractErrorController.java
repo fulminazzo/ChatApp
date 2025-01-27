3ca404a7ba1c87cf2534f469c17e9e28b5e0b528
user <fulminazzo21@gmail.com>
2025-01-27 22:12:21 +0100

    Created AbstractErrorController with internal ApiError

---
+++ src/main/java/it/fulminazzo/chatapp/backend/controllers/AbstractErrorController.java
@@ -0,0 +1,14 @@
+package it.fulminazzo.chatapp.backend.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public abstract class AbstractErrorController {
+
+    protected ResponseEntity<?> apiError(HttpStatus statusCode, String message) {
+        return new ResponseEntity<>(new ApiError(statusCode.value(), message), statusCode);
+    }
+
+    private record ApiError(int code, String message) {}
+
+}
