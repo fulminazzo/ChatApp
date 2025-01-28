a9ac92e8793fa959352d4bfae49588268935f844
user <fulminazzo21@gmail.com>
2025-01-28 19:50:25 +0100

    Created UserMapper to convert users and userdtos

---
+++ src/main/java/it/fulminazzo/chatapp/api/v1/mappers/UserMapper.java
@@ -0,0 +1,17 @@
+package it.fulminazzo.chatapp.api.v1.mappers;
+
+import it.fulminazzo.chatapp.api.v1.domain.dto.UserDto;
+import it.fulminazzo.chatapp.api.v1.domain.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto userToUserDto(User user);
+
+    User userDtoToUser(UserDto dto);
+
+}
