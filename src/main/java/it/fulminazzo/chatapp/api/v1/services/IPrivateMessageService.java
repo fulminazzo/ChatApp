01d028ad1e118b688a65e56630e233ddd8a725cb
user <fulminazzo21@gmail.com>
2025-01-28 23:15:07 +0100

    Added findByUserAndChat method

--- src/main/java/it/fulminazzo/chatapp/api/v1/services/IPrivateMessageService.java
+++ src/main/java/it/fulminazzo/chatapp/api/v1/services/IPrivateMessageService.java
@@ -1,4 +1,13 @@
 package it.fulminazzo.chatapp.api.v1.services;
 
+import it.fulminazzo.chatapp.api.v1.domain.dto.PrivateMessageDto;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface IPrivateMessageService {
+
+    List<PrivateMessageDto> findByUserAndChat(UUID userId, UUID chatId, Pageable pageable);
+
 }
--- src/main/java/it/fulminazzo/chatapp/api/v1/services/PrivateMessageService.java
+++ src/main/java/it/fulminazzo/chatapp/api/v1/services/PrivateMessageService.java
@@ -1,13 +1,37 @@
 package it.fulminazzo.chatapp.api.v1.services;
 
+import it.fulminazzo.chatapp.api.v1.domain.dto.PrivateChatDto;
+import it.fulminazzo.chatapp.api.v1.domain.dto.PrivateMessageDto;
+import it.fulminazzo.chatapp.api.v1.domain.entities.PrivateMessage;
+import it.fulminazzo.chatapp.api.v1.mappers.PrivateChatMapper;
+import it.fulminazzo.chatapp.api.v1.mappers.PrivateMessageMapper;
 import it.fulminazzo.chatapp.api.v1.repositories.PrivateMessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 class PrivateMessageService implements IPrivateMessageService {
 
     private final PrivateMessageRepository messageRepository;
+
+    private final IPrivateChatService privateChatService;
+
+    @Override
+    public List<PrivateMessageDto> findByUserAndChat(UUID userId, UUID chatId, Pageable pageable) {
+        PrivateChatDto chatDto = privateChatService.findOneByUser(userId, chatId);
+        List<PrivateMessage> messages = messageRepository.findAllByChat(
+                PrivateChatMapper.INSTANCE.privateChatDtoToPrivateChat(chatDto),
+                pageable
+        );
+        return messages.stream()
+                .map(PrivateMessageMapper.INSTANCE::privateMessageToPrivateMessageDto)
+                .toList();
+    }
 
 }
