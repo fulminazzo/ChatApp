326c4478a6f0bd7ec9de82005704529799753bec
user <fulminazzo21@gmail.com>
2025-01-28 20:00:22 +0100

    Fixed mapper not properly mapping dtos

--- src/main/java/it/fulminazzo/chatapp/api/v1/mappers/PrivateChatMapper.java
+++ src/main/java/it/fulminazzo/chatapp/api/v1/mappers/PrivateChatMapper.java
@@ -3,6 +3,7 @@ package it.fulminazzo.chatapp.api.v1.mappers;
 import it.fulminazzo.chatapp.api.v1.domain.dto.PrivateChatDto;
 import it.fulminazzo.chatapp.api.v1.domain.entities.PrivateChat;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,8 +11,12 @@ public interface PrivateChatMapper {
 
     PrivateChatMapper INSTANCE = Mappers.getMapper( PrivateChatMapper.class );
 
+    @Mapping(source = "firstUser", target = "first")
+    @Mapping(source = "secondUser", target = "second")
     PrivateChatDto privateChatToPrivateChatDto(PrivateChat privateChat);
 
+    @Mapping(source = "first", target = "firstUser")
+    @Mapping(source = "second", target = "secondUser")
     PrivateChat privateChatDtoToPrivateChat(PrivateChatDto privateChatDto);
 
 }
