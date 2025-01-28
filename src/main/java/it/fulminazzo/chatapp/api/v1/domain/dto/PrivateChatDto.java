afa3a77175f4930cc961b25a478a1c95cc149dc5
user <fulminazzo21@gmail.com>
2025-01-28 19:38:58 +0100

    Added checkUser method

--- src/main/java/it/fulminazzo/chatapp/api/v1/domain/dto/PrivateChatDto.java
+++ src/main/java/it/fulminazzo/chatapp/api/v1/domain/dto/PrivateChatDto.java
@@ -1,8 +1,10 @@
 package it.fulminazzo.chatapp.api.v1.domain.dto;
 
+import it.fulminazzo.chatapp.api.v1.exceptions.HttpException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 import java.util.UUID;
 
@@ -16,5 +18,22 @@ public class PrivateChatDto {
     private UserDto first;
 
     private UserDto second;
+
+    /**
+     * Checks whether the given userId matches with {@link #first#getId()}.
+     * If it does not, {@link #first} is flipped with {@link #second}.
+     *
+     * @param userId the user id
+     * @throws HttpException thrown in case the userId does not match with neither first nor second
+     */
+    public void checkUser(final UUID userId) throws HttpException {
+        if (!first.getId().equals(userId)) {
+            UserDto tmp = first;
+            first = second;
+            second = tmp;
+        }
+        if (!first.getId().equals(userId))
+            throw new HttpException(HttpStatus.FORBIDDEN);
+    }
 
 }
