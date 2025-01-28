b5b8a14761523becdc4ea7022ecf32c3c59d9e8e
user <fulminazzo21@gmail.com>
2025-01-28 20:19:52 +0100

    Created IUserService and implementation with findUserByIdOrThrow method

---
+++ src/main/java/it/fulminazzo/chatapp/api/v1/services/IUserService.java
@@ -0,0 +1,11 @@
+package it.fulminazzo.chatapp.api.v1.services;
+
+import it.fulminazzo.chatapp.api.v1.domain.entities.User;
+
+import java.util.UUID;
+
+public interface IUserService {
+
+    User findUserByIdOrThrow(UUID id);
+
+}
---
+++ src/main/java/it/fulminazzo/chatapp/api/v1/services/UserService.java
@@ -0,0 +1,23 @@
+package it.fulminazzo.chatapp.api.v1.services;
+
+import it.fulminazzo.chatapp.api.v1.domain.entities.User;
+import it.fulminazzo.chatapp.api.v1.exceptions.HttpException;
+import it.fulminazzo.chatapp.api.v1.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+class UserService implements IUserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User findUserByIdOrThrow(UUID id) {
+        return userRepository.findById(id).orElseThrow(() -> new HttpException(HttpStatus.NOT_FOUND));
+    }
+
+}
