ccc3be2df20c1bc237096a36d238ea2975a7694a
user <fulminazzo21@gmail.com>
2025-01-28 23:18:52 +0100

    Added get endpoint

--- src/main/java/it/fulminazzo/chatapp/api/v1/controllers/PrivateMessageController.java
+++ src/main/java/it/fulminazzo/chatapp/api/v1/controllers/PrivateMessageController.java
@@ -1,9 +1,14 @@
 package it.fulminazzo.chatapp.api.v1.controllers;
 
+import it.fulminazzo.chatapp.api.v1.domain.dto.PrivateMessageDto;
+import it.fulminazzo.chatapp.api.v1.domain.requests.GetPrivateMessageRequest;
 import it.fulminazzo.chatapp.api.v1.services.IPrivateMessageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/private/messages")
@@ -11,5 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class PrivateMessageController {
 
     private final IPrivateMessageService messageService;
+
+    @GetMapping
+    public List<PrivateMessageDto> getMessages(
+            @RequestAttribute UUID userId,
+            @RequestBody GetPrivateMessageRequest messageRequest,
+            Pageable pageable
+    ) {
+        return messageService.findByUserAndChat(userId, messageRequest.chatId(), pageable);
+    }
 
 }
