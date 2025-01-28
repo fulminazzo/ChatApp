1ddb7f4c7975fc72f86ae0ccc26b63c4a1ace339
user <fulminazzo21@gmail.com>
2025-01-28 23:15:19 +0100

    Added findAllByChat method

--- src/main/java/it/fulminazzo/chatapp/api/v1/repositories/PrivateMessageRepository.java
+++ src/main/java/it/fulminazzo/chatapp/api/v1/repositories/PrivateMessageRepository.java
@@ -1,12 +1,16 @@
 package it.fulminazzo.chatapp.api.v1.repositories;
 
+import it.fulminazzo.chatapp.api.v1.domain.entities.PrivateChat;
 import it.fulminazzo.chatapp.api.v1.domain.entities.PrivateMessage;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PrivateMessageRepository extends JpaRepository<PrivateMessage, UUID> {
 
+    List<PrivateMessage> findAllByChat(PrivateChat chat, Pageable pageable);
 }
