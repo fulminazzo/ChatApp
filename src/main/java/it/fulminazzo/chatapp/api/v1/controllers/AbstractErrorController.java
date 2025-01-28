cdb8c9ce4b773bf403353f04b42fd8214ce505c6
user <fulminazzo21@gmail.com>
2025-01-28 20:18:34 +0100

    Updated AbstractErrorController to not return message if empty

--- src/main/java/it/fulminazzo/chatapp/api/v1/controllers/AbstractErrorController.java
+++ src/main/java/it/fulminazzo/chatapp/api/v1/controllers/AbstractErrorController.java
@@ -3,12 +3,16 @@ package it.fulminazzo.chatapp.api.v1.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class AbstractErrorController {
 
     protected ResponseEntity<?> apiError(HttpStatus statusCode, String message) {
-        return new ResponseEntity<>(new ApiError(statusCode.value(), message), statusCode);
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", statusCode.value());
+        if (!message.isEmpty()) map.put("message", message);
+        return new ResponseEntity<>(map, statusCode);
     }
-
-    private record ApiError(int code, String message) {}
 
 }
