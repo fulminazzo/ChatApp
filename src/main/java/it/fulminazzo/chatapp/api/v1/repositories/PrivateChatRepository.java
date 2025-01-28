b14e8a471f82b274f00c17cde8aa04fde14ddef1
user <fulminazzo21@gmail.com>
2025-01-28 20:21:09 +0100

    Created PrivateChatRepository

---
+++ src/main/java/it/fulminazzo/chatapp/api/v1/repositories/PrivateChatRepository.java
@@ -0,0 +1,16 @@
+package it.fulminazzo.chatapp.api.v1.repositories;
+
+import it.fulminazzo.chatapp.api.v1.domain.entities.PrivateChat;
+import it.fulminazzo.chatapp.api.v1.domain.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PrivateChatRepository extends JpaRepository<PrivateChat, UUID> {
+
+    Page<PrivateChat> findAllByFirstUserOrSecondUser(User firstUser, User secondUser, Pageable pageable);
+}
